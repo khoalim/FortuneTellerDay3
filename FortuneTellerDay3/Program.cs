3d5a8a4074e0a302d9d6a956f22c911326c6fbc0
Kim Hoalim <kthoalim@gmail.com>
2019-09-05 16:33:19 -0400

    Initial Commit

---
+++ FortuneTellerDay3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FortuneTellerDay3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name?");
+
+            string firstName = Console.ReadLine();
+
+            Console.Clear();
+        }
+    }
+}
